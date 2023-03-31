8834972bbcfc52608e5a7178821871903dd35224
jaycedowell <jayce.dowell@gmail.com>
2023-03-31 07:55:27 -0600

    New linter exception.

--- tests/test_scripts.py
+++ tests/test_scripts.py
@@ -65,7 +65,8 @@ _SAFE_TO_IGNORE = ["Possible",
                    "Undefined variable 'useWisdom",
                    "Class 'int' has no 'from_bytes' member",
                    "Module 'astropy.units' has no 'hourangle' member",
-                   "Module 'astropy.units' has no 'degree' member"]
+                   "Module 'astropy.units' has no 'degree' member",
+                   "Instance of 'Empty' has no 'decode' member"]
 
 
 def _get_context(filename, line, before=0, after=0):
