f65e81c5e8e5bd96be5066b7d91b91abcbd5ad2e
jaycedowell <jaycedowell@gmail.com>
2024-09-18 16:05:35 -0600

    Let's try to clean this up a bit based on what is happening in LSL.

--- tests/test_scripts.py
+++ tests/test_scripts.py
@@ -18,7 +18,7 @@ run_scripts_tests = False
 try:
     from io import StringIO
     from pylint.lint import Run
-    from pylint.reporters.text import TextReporter
+    from pylint.reporters.json_reporter import JSONReporter
     if MODULE_BUILD is not None:
         run_scripts_tests = True
 except ImportError:
@@ -30,6 +30,9 @@ __author__   = "Jayce Dowell"
 
 
 _LINT_RE = re.compile('(?P<module>.*?):(?P<line>\d+): (error )?[\[\(](?P<type>.*?)[\]\)] (?P<info>.*)')
+
+
+_PYLINT_IGNORES = []
 
 
 _SAFE_TO_IGNORE = ["Possible",
@@ -77,83 +80,33 @@ def _get_context(filename, line, before=0, after=0):
 
 @unittest.skipUnless(run_scripts_tests, "requires the 'pylint' module")
 class scripts_tests(unittest.TestCase):
-    """A unittest.TestCase collection of unit tests for the pulsar scripts."""
+    """A unittest.TestCase collection of unit tests for the LSL scripts."""
     
-    pass     
-
-
-def _test_generator(script):
-    """
-    Function to build a test method for each script that is provided.  
-    Returns a function that is suitable as a method inside a unittest.TestCase
-    class
-    """
-    
-    def test(self):
-        pylint_output = StringIO()
-        reporter = TextReporter(pylint_output)
-        pylint_args = [script, "-E", "--extension-pkg-whitelist=numpy,ephem,lsl", "--init-hook='import sys; sys.path=[%s]; sys.path.insert(0, \"%s\")'" % (",".join(['"%s"' % p for p in sys.path]), os.path.dirname(MODULE_BUILD))]
-        Run(pylint_args, reporter=reporter, exit=False)
-        out = pylint_output.getvalue()
-        out_lines = out.split('\n')
+    def test_scripts(self):
+        """Static analysis of the LSL scripts."""
         
-        for line in out_lines:
-            ignore = False
-            for phrase in _SAFE_TO_IGNORE:
-                if line.find(phrase) != -1:
-                    ignore = True
-                    break
-            if ignore:
-                continue
+        _SCRIPTS = glob.glob(os.path.join(MODULE_BUILD, '..', 'scripts', '*.py'))
+        _SCRIPTS.sort()
+        for script in _SCRIPTS:
+            name = script.rsplit('scripts'+os.path.sep)[-1]
+            with self.subTest(script=name):
+                pylint_output = StringIO()
+                reporter = JSONReporter(pylint_output)
+                pylint_args = [script, "-E", "--extension-pkg-whitelist=numpy,ephem,lsl", "--init-hook='import sys; sys.path=[%s]; sys.path.insert(0, \"%s\")'" % (",".join(['"%s"' % p for p in sys.path]), os.path.dirname(MODULE_BUILD))]
+                Run(pylint_args, reporter=reporter, exit=False)
+                results = json.loads(pylint_output.getvalue())
                 
-            mtch = _LINT_RE.match(line)
-            if mtch is not None:
-                line_no, type, info = mtch.group('line'), mtch.group('type'), mtch.group('info')
-                ignore = False
-                if line.find('before assignment') != -1:
-                    context = _get_context(script, int(line_no), before=20, after=20)
-                    loc = context[20]
-                    level = len(loc) - len(loc.lstrip()) - 4
-                    if loc.strip().rstrip() == 'rawSpectraDedispersed)':
-                        ## This is fragile on purpose
-                        loc = context[20-4]
-                        level = len(loc) - len(loc.lstrip()) - 4
-                    found_try = None
-                    for i in range(1, 20):
-                        if context[20-i][level:level+3] == 'try':
-                            found_try = i
-                            break
-                    found_except = None
-                    for i in range(0, 20):
-                        if context[20+i][level:level+6] == 'except':
-                            found_except = i
-                            break
-                    if found_try is not None and found_except is not None:
-                        if context[20+found_except].find('NameError') != -1:
-                            ignore = True
-                if ignore:
-                    continue
-                    
-                self.assertEqual(type, None, "%s:%s - %s" % (os.path.basename(script), line_no, info))
-    return test
-
-
-if run_scripts_tests:
-    _SCRIPTS = glob.glob(os.path.join(MODULE_BUILD, '*.py'))
-    for depth in range(1, 3):
-        path = [MODULE_BUILD, '..']
-        path.extend(['*',]*depth)
-        path.append('*.py')
-        _SCRIPTS.extend(glob.glob(os.path.join(*path)))
-    _SCRIPTS = list(filter(lambda x: x.find('test_scripts.py') == -1, _SCRIPTS))
-    _SCRIPTS = list(filter(lambda x: x.find('psrfits_utils') == -1, _SCRIPTS))
-    _SCRIPTS.sort()
-    for script in _SCRIPTS:
-        test = _test_generator(script)
-        name = 'test_%s' % os.path.splitext(os.path.basename(script))[0]
-        doc = """Static analysis of the '%s' script.""" % os.path.basename(script)
-        setattr(test, '__doc__', doc)
-        setattr(scripts_tests, name, test)
+                for i,entry in enumerate(results):
+                    with self.subTest(error_number=i+1):
+                        false_positive = False
+                        for isym,imesg in _PYLINT_IGNORES:
+                            if entry['symbol'] == isym and entry['message'].startswith(imesg):
+                                false_positive = True
+                                break
+                        if false_positive:
+                            continue
+                            
+                        self.assertTrue(False, f"{entry['path']}:{entry['line']} - {entry['symbol']} - {entry['message']}")
 
 
 class scripts_test_suite(unittest.TestSuite):
